19f7afb2c0f83488cb547f1a4d3130d0f71ea8c8
Vincent No <ghzmdr@gmail.com>
2018-06-09 12:27:58 +0200

    [+]

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,8 +4,12 @@ import {loadState, saveState} from './data/persistence'
 import {createStore} from 'redux'
 
 const configureStore = _ => {
+
 	const persistedState = loadState()
+	// const persistedState = require('./test_data.json')
+
 	const store = createStore(root, persistedState)
+	// saveState(store.getState())
 
 	store.subscribe(_ => saveState(store.getState()))
 	return store
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -2,9 +2,27 @@ import { createSelector } from 'reselect'
 import Runaways from '../data/transforms/Runaways'
 
 const getBalance = ({balance}) => balance
-const getCosts = ({costs}) => costs
+const getCostsBurnRates = ({costs}) => costs.burnRates
 
 export const getRunaways = createSelector(
-	[getBalance, getCosts],
-	(balance, costs) => Runaways(costs.burnRates, balance)
+	[getBalance, getCostsBurnRates],
+	(balance, burnRates) => Runaways(burnRates, balance)
 )
+
+
+// export const getCostsAggregates = createSelector(
+// 	[getCostsEntries],
+// 	(entries) => Aggregates(entries)
+// )
+
+// export const getCostsBurnRates = createSelector(
+// 	[getCostsEntries, getCostsAggregates],
+// 	(entries) => Aggregates(entries)
+// )
+
+// export const getIncomesAggregates = createSelector(
+// 	[getIncomeEntries],
+// 	(entries) => Aggregates(entries)
+// )
+
+// export const getIncomesBurnRates = () => ({})
